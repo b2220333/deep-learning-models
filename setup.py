b233a55e315773160fca29559647d4535d641494
b2220333 <b2220333@gmail.com>
2019-08-20 16:40:01 +0800

    v0.2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='deep_learning_models',
+      version='0.1',
+      description='deep_learning_models',
+      author='Sam Lin',
+      author_email='hclin@ira.ee.ntu.edu.tw',
+      packages=find_packages()
+     )
+
